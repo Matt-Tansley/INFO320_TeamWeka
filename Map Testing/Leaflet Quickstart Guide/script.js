47cdbe1aa681f8c241d1a1b8fc2357e5d2538373
CrunchyPancakes <30matthewt@gmail.com>
2020-08-11 15:53:42 +1200

    More leaflet learning

---
+++ Map Testing/Leaflet Quickstart Guide/script.js
@@ -0,0 +1,63 @@
+var mymap = L.map("mapid").setView([51.505, -0.09], 13);
+
+L.tileLayer(
+  "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+  {
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: "mapbox/streets-v11",
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken:
+      "pk.eyJ1IjoiY3J1bmNoeXBhbmNha2VzIiwiYSI6ImNrM25temk0YzFzMjMzcHM3bWdocXZuOXgifQ.m6zMp4CxLPXi5xp-zB1kkg",
+  }
+).addTo(mymap);
+
+// Adds marker to map.
+var marker = L.marker([51.5, -0.09]).addTo(mymap);
+
+// Adds a circle to the map.
+var circle = L.circle([51.508, -0.11], {
+  color: "red",
+  fillColor: "#f03",
+  fillOpacity: 0.5,
+  radius: 500,
+}).addTo(mymap);
+
+// Adds a polygon to the map.
+var polygon = L.polygon([
+  [51.509, -0.08],
+  [51.503, -0.06],
+  [51.51, -0.047],
+]).addTo(mymap);
+
+// Add popups to the objects above.
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+circle.bindPopup("I am a circle.");
+polygon.bindPopup("I am a polygon.");
+
+// Creates a standalone popup on the map.
+var popup = L.popup()
+  .setLatLng([51.5, -0.09])
+  .setContent("I am a standalone popup.")
+  .openOn(mymap);
+
+// Event test
+// function onMapClick(e) {
+//   alert("You clicked the map at " + e.latlng);
+// }
+
+// mymap.on("click", onMapClick);
+
+// Improved event test (using a popup)
+var popup = L.popup();
+
+function onMapClick(e) {
+  popup
+    .setLatLng(e.latlng)
+    .setContent("You clicked the map at " + e.latlng.toString())
+    .openOn(mymap);
+}
+
+mymap.on("click", onMapClick);
