05b223be93a7f8d3f0d2ba69d891c9898ea5c52b
CrunchyPancakes <30matthewt@gmail.com>
2020-08-12 12:54:51 +1200

    App header + basic map

--- Sprint 1/Matt/script.js
+++ Sprint 1/Matt/script.js
@@ -1,3 +1,4 @@
+// Leaflet / Map related code
 var map = L.map("map").fitWorld();
 
 L.tileLayer(
@@ -15,22 +16,3 @@ L.tileLayer(
 ).addTo(map);
 
 map.locate({ setView: true, maxZoom: 16 });
-
-function onLocationFound(e) {
-  var radius = e.accuracy;
-
-  L.marker(e.latlng)
-    .addTo(map)
-    .bindPopup("You are within " + radius + " meters from this point")
-    .openPopup();
-
-  L.circle(e.latlng, radius).addTo(map);
-}
-
-map.on("locationfound", onLocationFound);
-
-function onLocationError(e) {
-  alert(e.message);
-}
-
-map.on("locationerror", onLocationError);
