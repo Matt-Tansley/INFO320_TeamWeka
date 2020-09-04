ebabcaee1591ed6c5c0d6e6f292fba05b73fa34c
CrunchyPancakes <30matthewt@gmail.com>
2020-09-04 12:33:33 +1200

    Testing map routing and starting to tidy up code in sprint 3 folder.

---
+++ Sprint 3/index.js
@@ -0,0 +1,9 @@
+const express = require("express"); // make sure we're using express!
+const app = express(); // make our 'app' an express one.
+const port = process.env.PORT || 3000; // Port that the server will run on. Will use a default available one, otherwise 3000.
+
+app.use(express.static(__dirname + "/src")); // make sure we are getting stuff from the src folder.
+
+app.get("/", (req, res) => res.sendFile(__dirname + "/src/login.html"));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
